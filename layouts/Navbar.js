cdd621be6ac0e582e2089862c446a710af1bf18f
Samuel Tettteh <samuelamoahtetteh@gmail.com>
2021-05-17 15:24:55 +0000

    second commit

--- layouts/Navbar.js
+++ layouts/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="bg-gray-600 text-white w-full py-4 ">
-      <div className="container mx-auto text-lg">Mfidie</div>
+      <div className="container p-4 mx-auto text-lg">Mfidie</div>
     </div>
   );
 }
